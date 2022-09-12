9787bc412d3006392d4a3ea243f3e121aa9386b8
slmnkankal <slmnknkl@gmail.com>
2022-09-12 11:57:48 +0300

    moment package added

--- src/components/flightsTable/FlightsTable.js
+++ src/components/flightsTable/FlightsTable.js
@@ -2,9 +2,10 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import Container from "react-bootstrap/Container";
 import Table from "react-bootstrap/Table";
+import Moment from "react-moment";
 
 const FlightsTable = () => {
-    const [flightsData, setFlightsData] = useState({ flightsBody: [] });
+  const [flightsData, setFlightsData] = useState({ flightsBody: [] });
 
   useEffect(() => {
     const fetchFlightsData = async () => {
@@ -16,36 +17,42 @@ const FlightsTable = () => {
     fetchFlightsData();
   }, [setFlightsData]);
   return (
-    <div><Container className="fluid">
-    <Table className="mt-5" striped>
-      <thead>
-        <tr>
-          <th>Id</th>
-          <th>Flight Number</th>
-          <th>Operation Airlines</th>
-          <th>Departure City</th>
-          <th>Arrival City</th>
-          <th>Date of Departure</th>
-          <th>Hour of Departure</th>
-        </tr>
-      </thead>
-      <tbody>
-        {flightsData.flightsBody &&
-          flightsData.flightsBody.map((item) => (
-            <tr key={item.id}>
-              <td>{item.id}</td>
-              <td>{item.flight_number}</td>
-              <td>{item.operation_airlines}</td>
-              <td>{item.departure_city}</td>
-              <td>{item.arrival_city}</td>
-              <td>{item.date_of_departure}</td>
-              <td>{item.etd}</td>
+    <div>
+      <Container className="fluid">
+        <Table className="mt-5" striped>
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>Flight Number</th>
+              <th>Operation Airlines</th>
+              <th>Departure City</th>
+              <th>Arrival City</th>
+              <th>Date of Departure</th>
+              <th>Hour of Departure</th>
             </tr>
-          ))}
-      </tbody>
-    </Table>
-  </Container></div>
-  )
-}
+          </thead>
+          <tbody>
+            {flightsData.flightsBody &&
+              flightsData.flightsBody.map((item) => (
+                <tr key={item.id}>
+                  <td>{item.id}</td>
+                  <td>{item.flight_number}</td>
+                  <td>{item.operation_airlines}</td>
+                  <td>{item.departure_city}</td>
+                  <td>{item.arrival_city}</td>
+                  <td>
+                    <Moment format="D MMM YYYY" withTitle>
+                      {item.date_of_departure}
+                    </Moment>
+                  </td>
+                  <td>{item.etd}</td>
+                </tr>
+              ))}
+          </tbody>
+        </Table>
+      </Container>
+    </div>
+  );
+};
 
-export default FlightsTable
+export default FlightsTable;
