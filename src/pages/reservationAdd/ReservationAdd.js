bdeab9a892c6a78d502b89f7c56c834f3f9831d5
slmnkankal <slmnknkl@gmail.com>
2022-09-14 13:25:56 +0300

    reservation form

--- src/pages/reservationAdd/ReservationAdd.js
+++ src/pages/reservationAdd/ReservationAdd.js
@@ -1,9 +1,95 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const ReservationAdd = () => {
-  return (
-    <div>ReservationAdd</div>
-  )
-}
 
-export default ReservationAdd
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+
+  return (
+    <>
+      <div className="d-flex justify-content-center mt-5">
+        <div className="form-image me-5">
+          <img src="https://picsum.photos/400/400" alt="sample-post" />
+        </div>
+        <div className="register-form">
+          <h1 className="form-title display-3">Passenger Info</h1>
+          <form id="register">
+            <div className="mb-3">
+              <select class="form-select" aria-label="Default select example">
+                <option selected>Open Flight Number</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+              </select>
+            </div>
+            <div className="mb-3">
+              <label htmlFor="name" className="form-label">
+                First Name
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="name"
+                placeholder="Enter your first name.."
+                value={firstName} // value={user.Email}
+                onChange={(e) => setFirstName(e.target.value)}
+                required
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="surname" className="form-label">
+                Last Name
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="surname"
+                placeholder="Enter your last name.."
+                value={lastName} // value={user.Email}
+                onChange={(e) => setLastName(e.target.value)}
+                required
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="email" className="form-label">
+                Email
+              </label>
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                placeholder="Enter your email address.."
+                value={email} // value={user.Email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="phone" className="form-label">
+                Phone Number
+              </label>
+              <input
+                type="number"
+                className="form-control"
+                id="phone"
+                placeholder="Enter your phone number.."
+                value={phone}
+                onChange={(e) => setPhone(e.target.value)}
+                required
+              />
+            </div>
+            <input
+              type="submit"
+              className="btn btn-primary form-control"
+              value="Submit Reservation"
+            />
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ReservationAdd;
