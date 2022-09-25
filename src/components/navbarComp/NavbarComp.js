65e971f8e6e434a215f246b420283ed6a68b9ecc
slmnkankal <slmnknkl@gmail.com>
2022-09-25 10:08:40 +0300

    message added to logout

--- src/components/navbarComp/NavbarComp.js
+++ src/components/navbarComp/NavbarComp.js
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
@@ -7,14 +7,29 @@ import HttpRequestService from "../../utils/HttpRequestService";
 import { useNavigate } from "react-router-dom";
 import { CgProfile } from "react-icons/cg";
 import { BiLogOut } from "react-icons/bi";
+import { Alert } from "react-bootstrap";
 
 const NavbarComp = () => {
   const user = useContext(UserContext);
   const userNameUpperCase = user.userDetails.username.toUpperCase();
 
+  const [alertOptions, setAlertOptions] = useState({
+    variant: null,
+    show: false,
+    message: "",
+  });
+
   const logoutData = {
     key: user.key,
     user: user.userDetails,
+  };
+
+  const manageAlertOptions = (variant, show, message) => {
+    setAlertOptions({
+      variant: variant,
+      show: show,
+      message: message,
+    });
   };
 
   let navigate = useNavigate();
@@ -26,13 +41,13 @@ const NavbarComp = () => {
       user.setUserEmail("");
       navigate("/login");
     } catch (error) {
-      
+      manageAlertOptions("danger", true, "You couldn't logged out!");
     }
   };
 
-const goReservations = () => {
-  navigate("/reservations");
-}
+  const goReservations = () => {
+    navigate("/reservations");
+  };
 
   return (
     <div>
@@ -41,7 +56,9 @@ const goReservations = () => {
           <Navbar.Brand href="#home">Flight Dashboard</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link onClick={goReservations} href="#">My Reservations</Nav.Link>
+            <Nav.Link onClick={goReservations} href="#">
+              My Reservations
+            </Nav.Link>
             <Nav.Link href="#pricing">Pricing</Nav.Link>
           </Nav>
           <Navbar.Toggle />
@@ -53,11 +70,22 @@ const goReservations = () => {
             </Navbar.Text>
           </Navbar.Collapse>
           <Nav className="mx-4">
-            <BiLogOut size="1.5em" color="rgba(255, 255, 255, 0.5" className="mt-2" />
+            <BiLogOut
+              size="1.5em"
+              color="rgba(255, 255, 255, 0.5"
+              className="mt-2"
+            />
             <Nav.Link onClick={() => handleLogout()}>Logout</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
+      <Alert
+        key={alertOptions.variant}
+        variant={alertOptions.variant}
+        show={alertOptions.show}
+      >
+        {alertOptions.message}
+      </Alert>
     </div>
   );
 };
